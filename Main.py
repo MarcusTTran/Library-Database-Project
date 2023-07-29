e42f1661be2316b4d49b79ee5b0c16ad2a1a1c6b
mtt8 <mtt8@sfu.ca>
2023-07-29 23:17:40 +0000

    Upload New File

---
+++ Main.py
@@ -0,0 +1,27 @@
+from TextUI import *
+from DatabaseManager import *
+
+import sqlite3
+from sqlite3 import Error
+
+
+
+
+
+
+def main():
+    print("Application Started")
+
+    print("Attempting connection to database...")
+    db_file = "library.db"
+
+
+
+    databaseManager = DatabaseManager()
+    databaseManager.create_connection(db_file)
+    ui = TextUI(databaseManager)
+    ui.start()
+
+
+if __name__ == "__main__":
+    main()
